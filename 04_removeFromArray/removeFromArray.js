df603e3aa6a8842f0afc0bea8b3dc7e95e033cc2
eduardsdn <eduard.sdn@gmail.com>
2022-07-11 17:11:02 +0200

    ex4 updated

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,25 @@
+
+
 const removeFromArray = function() {
 
-};
+    const array = arguments[0]
+    
+    for (let i=1; i < array.length; i++) {
+        
+        if (array.includes(arguments[i])) {
+            let index = array.indexOf(arguments[i]);
+            array.splice(index,1);
+        }
+        else {
+            continue
+        }
+    }
+
+    console.log(array)
+    return array
+}
+
+removeFromArray([1, 2, 3, 4], 1, 2, 3, 4)
 
 // Do not edit below this line
 module.exports = removeFromArray;
